7d826f6a04e106419110dcef3f909a3e244c1e91
Nika <ver.r762@gmail.com>
2024-10-01 12:11:52 +0300

    Update main

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
-#include "askname.h"
+#include <string.h>
 
 int main(int argc, char **argv)
 {
     char first[255], last[255];
+<<<<<<< HEAD
     askname(first, last);
     printf("Hello Nika%s %s!\n", first, last);
+=======
+    printf("Enter your first name: ");
+    fgets(first, 255, stdin);
+    first[strlen(first)-1] = '\0'; /* remove the newline at the end */
+    printf("Now enter your last name: ");
+    gets(last); /* buffer overflow? what's that? */
+    printf("Hello %s %s!\n", first, last);
+>>>>>>> parent of c181b2c (Move the ask name code into its own function and file.)
     return 0;
 }
